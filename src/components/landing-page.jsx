6891147ddbd9341fcacce320675223e534b3f4b4
Rahul Dutt <d92.rahul@gmail.com>
2024-03-26 22:26:03 +1200

    Name CHange

--- src/components/landing-page.jsx
+++ src/components/landing-page.jsx
@@ -11,7 +11,7 @@ export function LandingPage() {
   return (
     (<div className="flex flex-col min-h-[100dvh]">
       <header className="px-4 lg:px-6 h-14 flex items-center">
-        <div className="flex items-center justify-center">Concrete Inc</div>
+        <div className="flex items-center justify-center">Joe&apos;s Concrete Inc</div>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link
             className="text-sm font-medium hover:underline underline-offset-4"
