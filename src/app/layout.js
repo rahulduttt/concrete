623a5f06e9d7868e19521a5e7c766ffbc7746fcd
Rahul Dutt <d92.rahul@gmail.com>
2024-11-06 02:34:44 +1200

    feat: Upgrade Next.js to 15.0.2, React to 19.0.0-rc, add ThemeProvider for theming support in layout and home components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const lato = Poppins({
   weight: ["100","200","500","300", "800","400", "700"],
   style: ["normal", "italic"],
   subsets: ["latin"],
 });
+
 export const metadata = {
   title: "Wise Concreting Services",
   description: "Wise Concreting Services",
@@ -13,8 +15,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={lato.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={lato.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,11 @@
 import { LandingPageV2 } from "@/components/landing-page-v2";
-import { ThemeProvider } from "@/components/ThemeProvider";
-import Image from "next/image";
 import { Toaster } from "@/components/ui/toaster"
 
 export default function Home() {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
+    <>
       <LandingPageV2 />
       <Toaster />
-    </ThemeProvider>
+    </>
   );
 }
